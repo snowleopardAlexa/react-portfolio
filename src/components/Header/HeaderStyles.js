f48a601e2c1474acb78eb8e5f9172e1950085fc0
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-09 10:06:05 -0400

    add Header and styles

---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 2rem;
+  padding: 1rem;
+  padding-top: 2rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(2, 60px);
+    grid-column-gap: 0.5rem;
+    grid-row-gap: 0.5rem;
+  }
+`;
+export const Div1 = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    grid-area: 1 / 1 / 2 / 3;
+  }
+`;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { Layout } from '../layout/Layout';
+
+const Home = () => {
+  return (
+    <Layout />
+  );
+};
+
+export default Home;
