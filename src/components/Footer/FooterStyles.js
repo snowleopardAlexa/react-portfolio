7fdaec5dc3683038b334be62ec40e3ccd7fd7f28
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-10 14:05:32 -0400

    add icon logo

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const FooterWrapper = styled.section`
+export const FooterContainer = styled.section`
   width: calc(100vw - 96px);
   max-width: 1040px;
   padding: 2rem 48px 40px;
